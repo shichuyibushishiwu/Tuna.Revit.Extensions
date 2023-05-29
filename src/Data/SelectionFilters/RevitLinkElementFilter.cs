9840454122c429e449c30819fdb649e14d8dee21
Yandong <yandong@qq.com>
2023-05-29 22:31:07 +0800

    modify selection filter errors

--- src/Data/SelectionFilters/RevitLinkElementFilter.cs
+++ src/Data/SelectionFilters/RevitLinkElementFilter.cs
@@ -6,74 +6,72 @@ using System.Linq;
 
 namespace Tuna.Revit.Extension.Data.SelectionFilters
 {
-    public static partial class SelectionFilterExtension
+
+    /// <summary>
+    /// this class is used to filter element in link document object class is the same as input class 
+    /// 链接构件过滤器，用于选择时过滤指定链接文件中的指定类型的构件
+    /// </summary>
+    /// <typeparam name="TElement"></typeparam>
+    public class RevitLinkElementFilter<TElement> : ISelectionFilter where TElement : Element
     {
+        private readonly IEnumerable<RevitLinkInstance> _linkInstances;
+        private readonly Func<TElement, bool> _filterCondition;
+
         /// <summary>
-        /// this class is used to filter element in link document object class is the same as input class 
-        /// 链接构件过滤器，用于选择时过滤指定链接文件中的指定类型的构件
+        /// 构造函数
         /// </summary>
-        /// <typeparam name="TElement"></typeparam>
-        public class RevitLinkElementFilter<TElement> : ISelectionFilter where TElement : Element
+        /// <param name="linkInstances">指定过滤的链接模型</param>
+        /// <param name="filterCondition">自定义目标类别构件的过滤条件</param>
+        public RevitLinkElementFilter(IEnumerable<RevitLinkInstance> linkInstances = null, Func<TElement, bool> filterCondition = null)
         {
-            private readonly IEnumerable<RevitLinkInstance> _linkInstances;
-            private readonly Func<TElement, bool> _filterCondition;
+            _linkInstances = linkInstances;
+            _filterCondition = filterCondition;
+        }
 
-            /// <summary>
-            /// 构造函数
-            /// </summary>
-            /// <param name="linkInstances">指定过滤的链接模型</param>
-            /// <param name="filterCondition">自定义目标类别构件的过滤条件</param>
-            public RevitLinkElementFilter(IEnumerable<RevitLinkInstance> linkInstances = null, Func<TElement, bool> filterCondition = null)
+
+        public bool AllowElement(Element elem)
+        {
+            if (_linkInstances == null)
             {
-                _linkInstances = linkInstances;
-                _filterCondition = filterCondition;
+                return true;
             }
-
-
-            public bool AllowElement(Element elem)
+            else
             {
-                if (_linkInstances == null)
+                var resultLinkInstance = _linkInstances.Where(x => x.Id == elem.Id);
+                //指定选择的元素位于哪些链接模型中
+                if (resultLinkInstance != null && resultLinkInstance.Any())
                 {
                     return true;
                 }
-                else
-                {
-                    var resultLinkInstance = _linkInstances.Where(x => x.Id == elem.Id);
-                    //指定选择的元素位于哪些链接模型中
-                    if (resultLinkInstance != null && resultLinkInstance.Any())
-                    {
-                        return true;
-                    }
-                    return false;
-                }
-            }
-
-            public bool AllowReference(Reference reference, XYZ position)
-            {
-                foreach (var linkInstance in _linkInstances)
-                {
-                    try
-                    {
-                        var element = linkInstance.GetLinkDocument()?.GetElement(reference.LinkedElementId) is TElement tElement ? tElement : null;
-                        if (element == null && element.IsValidObject != true)
-                        {
-                            element = null;
-                        }
-                        if (_filterCondition != null && !_filterCondition.Invoke(element))
-                        {
-                            element = null;
-                        }
-                        if (element != null)
-                        { return true; }
-                        return false;
-                    }
-                    catch (Exception)
-                    {
-                        continue;
-                    }
-                }
                 return false;
             }
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            foreach (var linkInstance in _linkInstances)
+            {
+                try
+                {
+                    var element = linkInstance.GetLinkDocument()?.GetElement(reference.LinkedElementId) is TElement tElement ? tElement : null;
+                    if (element == null && element.IsValidObject != true)
+                    {
+                        element = null;
+                    }
+                    if (_filterCondition != null && !_filterCondition.Invoke(element))
+                    {
+                        element = null;
+                    }
+                    if (element != null)
+                    { return true; }
+                    return false;
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+            return false;
         }
     }
 }
--- src/Data/SelectionFilters/RevitLinkGeometryFilter.cs
+++ src/Data/SelectionFilters/RevitLinkGeometryFilter.cs
@@ -6,74 +6,71 @@ using System.Linq;
 
 namespace Tuna.Revit.Extension.Data.SelectionFilters
 {
-    public static partial class SelectionFilterExtension
+    /// <summary>
+    /// this class is used to filter element in link document object class is the same as input class 
+    /// 链接构件过滤器，用于选择时过滤指定链接文件中的指定类型的构件
+    /// </summary>
+    /// <typeparam name="TElement"></typeparam>
+    public class RevitLinkGeometryFilter<TGeometryObject> : ISelectionFilter where TGeometryObject : GeometryObject
     {
+        private readonly IEnumerable<RevitLinkInstance> _linkInstances;
+        private readonly Func<TGeometryObject, bool> _filterCondition;
+
         /// <summary>
-        /// this class is used to filter element in link document object class is the same as input class 
-        /// 链接构件过滤器，用于选择时过滤指定链接文件中的指定类型的构件
+        /// 构造函数
         /// </summary>
-        /// <typeparam name="TElement"></typeparam>
-        public class RevitLinkGeometryFilter<TGeometryObject> : ISelectionFilter where TGeometryObject : GeometryObject
+        /// <param name="linkInstances">指定过滤的链接模型</param>
+        /// <param name="filterCondition">自定义目标类别构件的过滤条件</param>
+        public RevitLinkGeometryFilter(IEnumerable<RevitLinkInstance> linkInstances = null, Func<TGeometryObject, bool> filterCondition = null)
         {
-            private readonly IEnumerable<RevitLinkInstance> _linkInstances;
-            private readonly Func<TGeometryObject, bool> _filterCondition;
+            _linkInstances = linkInstances;
+            _filterCondition = filterCondition;
+        }
 
-            /// <summary>
-            /// 构造函数
-            /// </summary>
-            /// <param name="linkInstances">指定过滤的链接模型</param>
-            /// <param name="filterCondition">自定义目标类别构件的过滤条件</param>
-            public RevitLinkGeometryFilter(IEnumerable<RevitLinkInstance> linkInstances = null, Func<TGeometryObject, bool> filterCondition = null)
+
+        public bool AllowElement(Element elem)
+        {
+            if (_linkInstances == null)
             {
-                _linkInstances = linkInstances;
-                _filterCondition = filterCondition;
+                return true;
             }
-
-
-            public bool AllowElement(Element elem)
+            else
             {
-                if (_linkInstances == null)
+                var resultLinkInstance = _linkInstances.Where(x => x.Id == elem.Id);
+                //指定选择的元素位于哪些链接模型中
+                if (resultLinkInstance != null && resultLinkInstance.Any())
                 {
                     return true;
                 }
-                else
-                {
-                    var resultLinkInstance = _linkInstances.Where(x => x.Id == elem.Id);
-                    //指定选择的元素位于哪些链接模型中
-                    if (resultLinkInstance != null && resultLinkInstance.Any())
-                    {
-                        return true;
-                    }
-                    return false;
-                }
-            }
-
-            public bool AllowReference(Reference reference, XYZ position)
-            {
-                foreach (var linkInstance in _linkInstances)
-                {
-                    try
-                    {
-                        var geometryObject = linkInstance.GetLinkDocument()?.GetElement(reference.LinkedElementId).GetGeometryObjectFromReference(reference) is TGeometryObject tElement ? tElement : null;
-                        if (geometryObject == null )
-                        {
-                            geometryObject = null;
-                        }
-                        if (_filterCondition != null && !_filterCondition.Invoke(geometryObject))
-                        {
-                            geometryObject = null;
-                        }
-                        if (geometryObject != null)
-                        { return true; }
-                        return false;
-                    }
-                    catch (Exception)
-                    {
-                        continue;
-                    }
-                }
                 return false;
             }
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            foreach (var linkInstance in _linkInstances)
+            {
+                try
+                {
+                    var geometryObject = linkInstance.GetLinkDocument()?.GetElement(reference.LinkedElementId).GetGeometryObjectFromReference(reference) is TGeometryObject tElement ? tElement : null;
+                    if (geometryObject == null)
+                    {
+                        geometryObject = null;
+                    }
+                    if (_filterCondition != null && !_filterCondition.Invoke(geometryObject))
+                    {
+                        geometryObject = null;
+                    }
+                    if (geometryObject != null)
+                    { return true; }
+                    return false;
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+            return false;
         }
     }
 }
