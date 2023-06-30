3e5b19d891e6b156b0cba534adc1606f4e3573b1
Shiwu <49446319+shichuyibushishiwu@users.noreply.github.com>
2023-06-30 18:32:54 +0800

    Add External Event Service

---
+++ src/Services/ExternalEventService.cs
@@ -0,0 +1,33 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuna.Revit.Extension.Services
+{
+    internal class ExternalEventService : IExternalEventService, IExternalEventHandler
+    {
+        private readonly ExternalEvent _externalEvent;
+
+        public ExternalEventService()
+        {
+            _externalEvent = ExternalEvent.Create(this);
+        }
+
+        public void Execute(UIApplication app)
+        {
+
+        }
+
+        public string GetName() => "Tuna external event";
+
+        public void Run()
+        {
+            ExternalEventRequest request = _externalEvent.Raise();
+
+
+        }
+    }
+}
