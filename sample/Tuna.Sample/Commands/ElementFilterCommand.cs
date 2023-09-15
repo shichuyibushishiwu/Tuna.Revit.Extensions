4084a00c7551893d9ad72d499a59f75fdef3b3f2
Shiwu <49446319+shichuyibushishiwu@users.noreply.github.com>
2023-09-16 00:00:27 +0800

    Update Tuna.Sample.csproj

--- sample/Tuna.Sample/Commands/ElementFilterCommand.cs
+++ sample/Tuna.Sample/Commands/ElementFilterCommand.cs
@@ -10,100 +10,91 @@
 
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
-using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.DB.Structure;
 using Autodesk.Revit.UI;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 using Tuna.Revit.Extension;
 using Tuna.Revit.Extension.Constants;
 
-namespace Tuna.Sample.Commands
+namespace Tuna.Sample.Commands;
+
+[Transaction(TransactionMode.Manual)]
+public class ElementFilterCommand : IExternalCommand
 {
-    [Transaction(TransactionMode.Manual)]
-    public class ElementFilterCommand : IExternalCommand
+    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
     {
-        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        UIDocument uIDocument = commandData.Application.ActiveUIDocument;
+        Document document = uIDocument.Document;
+
+
+        //这些方法可以获取到项目中的图元，不包括图元类型，所以如果在使用 BuiltInCategory 的时候，不需要再对类型进行过滤
+        document.GetElements<Wall>();
+
+        document.GetElements(typeof(Wall));
+
+        document.GetElements(typeof(Wall), typeof(Floor));
+
+        document.GetElements(new ElementClassFilter(typeof(Wall)));
+
+        document.GetElements(BuiltInCategories.Wall);
+
+        document.GetElements(BuiltInCategory.OST_Walls);
+
+        document.GetElements(BuiltInCategory.OST_Walls, BuiltInCategory.OST_WallsCutPattern);
+
+        document.GetElements(StructuralWallUsage.Bearing);
+
+        document.GetElements(StructuralMaterialType.Steel);
+
+        document.GetElements(StructuralInstanceUsage.Column);
+
+        document.GetElements(StructuralType.Column);
+
+        document.GetElements(CurveElementType.ModelCurve);
+
+        document.GetElements(document.GetFamilySymbols(document.GetElements(typeof(Family)).FirstElementId()).FirstOrDefault());
+
+        document.GetElements(document.GetElements<Level>().FirstOrDefault());
+
+
+        //这些方法可以获取到项目中的类型
+        document.GetElementTypes(BuiltInCategories.Wall);
+
+        document.GetElementTypes(BuiltInCategory.OST_Walls);
+
+        document.GetElementTypes<WallType>();
+
+
+        //这个方法可以获取结构相关的族
+        document.GetStructualFamilies(StructuralMaterialType.Steel);
+
+
+        var result = document.GetElements("固定", "900*2100");
+        if (result.Succeeded)
         {
-            UIDocument uIDocument = commandData.Application.ActiveUIDocument;
-            Document document = uIDocument.Document;
-
-
-            //这些方法可以获取到项目中的图元，不包括图元类型，所以如果在使用 BuiltInCategory 的时候，不需要再对类型进行过滤
-            document.GetElements<Wall>();
-
-            document.GetElements(typeof(Wall));
-
-            document.GetElements(typeof(Wall), typeof(Floor));
-
-            document.GetElements(new ElementClassFilter(typeof(Wall)));
-
-            document.GetElements(BuiltInCategories.Wall);
-
-            document.GetElements(BuiltInCategory.OST_Walls);
-
-            document.GetElements(BuiltInCategory.OST_Walls, BuiltInCategory.OST_WallsCutPattern);
-
-            document.GetElements(StructuralWallUsage.Bearing);
-
-            document.GetElements(StructuralMaterialType.Steel);
-
-            document.GetElements(StructuralInstanceUsage.Column);
-
-            document.GetElements(StructuralType.Column);
-
-            document.GetElements(CurveElementType.ModelCurve);
-
-            document.GetElements(document.GetFamilySymbols(document.GetElements(typeof(Family)).FirstElementId()).FirstOrDefault());
-
-            document.GetElements(document.GetElements<Level>().FirstOrDefault());
-
-
-            //这些方法可以获取到项目中的类型
-            document.GetElementTypes(BuiltInCategories.Wall);
-
-            document.GetElementTypes(BuiltInCategory.OST_Walls);
-
-            document.GetElementTypes<WallType>();
-
-
-            //这个方法可以获取结构相关的族
-            document.GetStructualFamilies(StructuralMaterialType.Steel);
-
-
-            var result = document.GetElements("固定", "900*2100");
-            if (result.Succeeded)
-            {
-                FilteredElementCollector windows = result.Value;
-            }
-
-            var elems = uIDocument.ActiveGraphicalView.GetElements(BuiltInCategory.OST_Walls);
-
-
-            //document.GetElements()
-            //    .OfCategories(BuiltInCategory.OST_Walls)
-            //    .WithParameterStringValue(BuiltInParameter.ALL_MODEL_MODEL, ParameterFilterStringOperator.BeginWith, "m")
-            //    .WithParameterStringValue(BuiltInParameter.ALL_MODEL_COST, ParameterFilterStringOperator.Contains, "m")
-            //    .WithParameterStringValue(BuiltInParameter.ALL_MODEL_FAMILY_NAME, ParameterFilterStringOperator.BeginWith, "m")
-            //    .WithParameterStringValue(BuiltInParameter.ALLOW_AUTO_EMBED, ParameterFilterStringOperator.BeginWith, "m")
-            //    .ToFilter()
-            //    .ToElements();
-
-
-
-            //document.GetElements<FamilyInstance>(instance => instance.Symbol.FamilyName == "预制");
-
-            //var elems = document.GetGraphicElements<FamilyInstance>(instance => instance.Name == "name");
-
-            //Enumerator.Range(1, 100);
-
-            return Result.Succeeded;
+            FilteredElementCollector windows = result.Value;
         }
+
+        var elems = uIDocument.ActiveGraphicalView.GetElements(BuiltInCategory.OST_Walls);
+
+
+        //document.GetElements()
+        //    .OfCategories(BuiltInCategory.OST_Walls)
+        //    .WithParameterStringValue(BuiltInParameter.ALL_MODEL_MODEL, ParameterFilterStringOperator.BeginWith, "m")
+        //    .WithParameterStringValue(BuiltInParameter.ALL_MODEL_COST, ParameterFilterStringOperator.Contains, "m")
+        //    .WithParameterStringValue(BuiltInParameter.ALL_MODEL_FAMILY_NAME, ParameterFilterStringOperator.BeginWith, "m")
+        //    .WithParameterStringValue(BuiltInParameter.ALLOW_AUTO_EMBED, ParameterFilterStringOperator.BeginWith, "m")
+        //    .ToFilter()
+        //    .ToElements();
+
+
+
+        //document.GetElements<FamilyInstance>(instance => instance.Symbol.FamilyName == "预制");
+
+        //var elems = document.GetGraphicElements<FamilyInstance>(instance => instance.Name == "name");
+
+        //Enumerator.Range(1, 100);
+
+        return Result.Succeeded;
     }
-
-
 }
