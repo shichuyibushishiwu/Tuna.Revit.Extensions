63a5093fb9d3feb24375f9cd4c83135e2911d1d1
shichuyibushishiwu <49446319+shichuyibushishiwu@users.noreply.github.com>
2024-01-22 16:49:20 +0800

    Update TransactionExtensions.cs

--- src/Transaction/TransactionExtensions.cs
+++ src/Transaction/TransactionExtensions.cs
@@ -8,36 +8,24 @@ using System.Threading.Tasks;
 namespace Tuna.Revit.Extension;
 
 /// <summary>
-/// 
+/// 事务的扩展
 /// </summary>
 public static class TransactionExtensions
 {
-    /// 从当前文档开启一个事务，以便于执行对文档修改的操作
     /// <summary>
     /// 从当前文档开启一个事务，以便于执行对文档修改的操作
     /// <para>Start a revit database transaction from the target document in order to modify document</para>
-    ///<example>
-    /// show how to used current method
-    /// <code>
-    /// document.NewTransaction((doc)=>
-    /// {
-    ///     //todo
-    /// }
-    /// </code>
-    /// </example>
     /// </summary>
     /// <param name="document"></param>
     /// <param name="action"></param>
     /// <param name="name"></param>
-    /// <returns>If document is read only,return <see cref="Autodesk.Revit.DB.TransactionStatus.Error"/></returns>
-    public static TransactionResult NewTransaction(this Document document, Action action, string name = "Default Transaction Name")
+    /// <returns><see cref="TransactionResult"/></returns>
+    public static TransactionResult NewTransaction(this Document document, Action<FailureHandlingOptions> action, string name = "Default Transaction Name")
     {
         ArgumentNullExceptionUtils.ThrowIfNullOrInvalid(document);
         ArgumentNullExceptionUtils.ThrowIfNull(action);
 
-
         TransactionResult result = new TransactionResult();
-
         if (document.IsReadOnly)
         {
             result.TransactionStatus = TransactionStatus.Error;
@@ -45,34 +33,48 @@ public static class TransactionExtensions
             return result;
         }
 
-        using (Transaction transaction = new Transaction(document, name))
+        using (Transaction transaction = new Transaction(document))
         {
-            result.TransactionStatus = transaction.Start();
+            result.TransactionStatus = transaction.Start(name);
             if (result.TransactionStatus != TransactionStatus.Started)
             {
-                result.Message = $"{document} is read only";
+                result.Message = $"{document} is not started";
                 return result;
             }
 
+            var options = transaction.GetFailureHandlingOptions();
             try
             {
-                action.Invoke();
+                action.Invoke(options);
+                result.TransactionStatus = transaction.Commit(options);
                 return result;
             }
             catch (TransactionRollbackException exception)
             {
-                transaction.RollBack(transaction.GetFailureHandlingOptions().SetClearAfterRollback(true));
                 result.Message = exception.Message;
-
+                result.TransactionStatus = transaction.RollBack(options);
                 return result;
             }
             catch (Exception exception)
             {
                 result.Exception = exception;
                 result.Message = exception.Message;
-                transaction.RollBack();
-                return result;
+                result.TransactionStatus = transaction.RollBack(options);
+                throw exception;
             }
         }
+    }
+
+    /// <summary>
+    /// 从当前文档开启一个事务，以便于执行对文档修改的操作
+    /// <para>Start a revit database transaction from the target document in order to modify document</para>
+    /// </summary>
+    /// <param name="document"></param>
+    /// <param name="action"></param>
+    /// <param name="name"></param>
+    /// <returns>If document is read only,return <see cref="Autodesk.Revit.DB.TransactionStatus.Error"/></returns>
+    public static TransactionResult NewTransaction(this Document document, Action action, string name = "Default Transaction Name")
+    {
+        return document.NewTransaction(options => action(), name);
     }
 }
