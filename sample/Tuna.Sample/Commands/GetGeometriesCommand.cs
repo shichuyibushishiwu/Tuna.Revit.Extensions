e20906e5b4cc8204f8b47d65f11fa70a1df12be1
SamllPigYanDong <353554036@qq.COM>
2024-07-19 23:02:06 +0800

    几何扩展以及几何扩展用例的添加

---
+++ sample/Tuna.Sample/Commands/GetGeometriesCommand.cs
@@ -0,0 +1,49 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Tuna.Revit.Extension;
+
+namespace Tuna.Sample.Commands
+{
+    /// <summary>
+    /// 获取几何信息的演示样例
+    /// 此处先使用扩展获取了几何块，再对几何块进行进一步的解析；
+    /// </summary>
+    [Transaction(TransactionMode.Manual)]
+    public class GetGeometriesCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uiDocument = commandData.Application.ActiveUIDocument;
+
+            Document document = uiDocument.Document;
+
+            var element = uiDocument.SelectElement().Value;
+            //get solids before get faces
+            var solids = GeometryExtensions.ResolveSolid(element, (option) => { 
+               option.DetailLevel=ViewDetailLevel.Fine;
+            });
+            //get faces from solids 
+            var faces = solids.SelectMany<Solid, Face>((solid) =>
+             {
+                 var faces = new List<Face>();
+                 foreach (Face face in solid.Faces)
+                 {
+                     faces.Add(face);
+                 }
+                 return faces;
+             });
+            //show result faces count
+            MessageBox.Show(faces.Count().ToString());
+
+
+            return Result.Succeeded;
+        }
+    }
+}
--- src/Geometry/GeometryExtensions.cs
+++ src/Geometry/GeometryExtensions.cs
@@ -14,6 +14,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using Tuna.Revit.Extension.Geometry;
 
 namespace Tuna.Revit.Extension;
 
@@ -23,8 +25,10 @@ namespace Tuna.Revit.Extension;
 public static class GeometryExtensions
 {
     /// <summary>
-    /// 解析图形
+    /// 解析几何
     /// </summary>
+    /// <param name="element">解析的几何对象</param>
+    /// <param name="action">设定获取几何的选项</param>
     public static List<GeometryObject> ResolveGeometry(this Element element, Action<Options> action)
     {
         ArgumentNullExceptionUtils.ThrowIfNullOrInvalid(element);
@@ -35,6 +39,47 @@ public static class GeometryExtensions
 
         return ResolveGeometry(geometries);
     }
+
+
+    /// <summary>
+    /// 解析几何，并可定义几何结果类型及目标几何来源
+    /// </summary>
+    /// <typeparam name="T">几何结果类型</typeparam>
+    /// <param name="element">解析的几何对象</param>
+    /// <param name="action">设定获取几何的选项</param>
+    /// <param name="geometryType">目标几何来源</param>
+    /// <returns>几何结果集合</returns>
+    public static List<T> ResolveGeometry<T>(this Element element, Action<Options> action, GeometryType geometryType= GeometryType.Instance) where T : GeometryObject
+    {
+        ArgumentNullExceptionUtils.ThrowIfNullOrInvalid(element);
+
+        Options options = new Options();
+        action.Invoke(options);
+        GeometryElement geometries = element.get_Geometry(options);
+
+        return ResolveGeometry<T>(geometries, geometryType);
+    }
+
+
+    /// <summary>
+    /// 解析几何块
+    /// </summary>
+    /// <param name="element">解析的几何对象</param>
+    /// <param name="action">设定获取几何的选项</param>
+    /// <param name="geometryType">目标几何来源</param>
+    /// <returns>几何结果集合</returns>
+    public static List<Solid> ResolveSolid(this Element element, Action<Options> action, GeometryType geometryType = GeometryType.Instance) 
+    {
+        ArgumentNullExceptionUtils.ThrowIfNullOrInvalid(element);
+
+        Options options = new Options();
+        action.Invoke(options);
+        GeometryElement geometries = element.get_Geometry(options);
+
+        return ResolveGeometry<Solid>(geometries, geometryType);
+    }
+
+
 
     private static List<GeometryObject> ResolveGeometry(this GeometryElement geometries)
     {
@@ -57,4 +102,37 @@ public static class GeometryExtensions
         return objects;
     }
 
+
+    private static List<T> ResolveGeometry<T>(this GeometryElement geometries, GeometryType geometryType = GeometryType.Instance) where T : GeometryObject 
+    {
+        var results = new List<T>();
+        foreach (GeometryObject item in geometries)
+        {
+            switch (item)
+            {
+                case T targetType:
+                    results.Add(targetType);
+                    break;
+                case GeometryInstance geomInstance:
+                    var geometry = geomInstance.GetInstanceGeometry();
+                    if (geometryType == GeometryType.Symbol)
+                    {
+                        geometry = geomInstance.GetSymbolGeometry();
+                    }
+                    results.AddRange(ResolveGeometry<T>(geometry, geometryType));
+                    break;
+                case GeometryElement geometryElement:
+                    results.AddRange(ResolveGeometry<T>(geometryElement, geometryType));
+                    break;
+                default:
+                    break;
+            }
+        }
+        return results;
+    }
+
+
+
+
 }
+
